db59aa1550e13036e1418c880ccdb2231add57a9
GowriMani <gowrimani2609@gmail.com>
2021-10-06 23:17:17 +0530

    Stone

---
+++ src/test/java/vtigerPractice/Demo.java
@@ -0,0 +1,23 @@
+package vtigerPractice;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+	@Test
+	public void add(){
+		System.out.println("GooD EVEning");
+
+	}
+	
+	@Test
+	public void sub() {
+		System.out.println("ALL THE BEST");
+	}
+	
+	@Test
+	public void all() {
+		System.out.println("ALL is WEll");
+		
+	}
+
+}
